1f193362667c79f34da331629669fdbb1df22251
Herususeno56 <87887293+Herususeno56@users.noreply.github.com>
2022-10-03 16:21:11 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Navbar from './Navbar';
+import Navbaar from './Navbaar';
+import Navbaaar from './Navbaaar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      HERU SUSENO
+   <Navbar />
+   <Navbaar />
+   <Navbaaar />
     </div>
+   
   );
 }
+
 
 export default App;
